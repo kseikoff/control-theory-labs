90168d1cfe5345c14d24c292d5032c9705f7e474
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2025-02-22 22:38:48 +0300

    code lab 1 task 1 part 1

--- act_lab1/task_1.m
+++ act_lab1/task_1.m
@@ -1,8 +1,24 @@
+% input data
 A = [1 -2 3; 2 -3 2; -2 1 -4];
 B = [-3; -1; 3];
+x1 = [4; 3; -3];
 t1 = 3;
 
-% Gramian
+% controllability matrix
+U = [B A*B A*A*B];
+r = rank(U);
+disp(U);
+disp(r);
+
+% A matrix eigenvalues
+A_e = eig(A);
+disp(A_e);
+
+% gramian
 integrand = @(t) expm(A * t) * (B * B') * expm(A' * t);
 P_t1 = integral(@(t) integrand(t), 0, t1, 'ArrayValued', true);
 disp(P_t1);
+
+% gramian eigenvalues
+e = eig(P_t1);
+disp(e);

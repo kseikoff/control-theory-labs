dc8831d9fbdb1bdaaa07edbc86429d0b90904341
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2025-04-11 14:45:17 +0300

    lab 4 task 1-2

--- act_lab4/src/scripts/task_2.m
+++ act_lab4/src/scripts/task_2.m
@@ -3,9 +3,7 @@ A = [5 2 7;
      2 1 2;
     -2 -3 -4];
 B = [3; 1; -1];
-Bf = [-4 0 0 -1;
-      0 0 0 0;
-      4 0 0 0];
+Bg = 0;
 
 G = [25 6 -20 11;
      14 3 -10 4;
@@ -13,22 +11,7 @@ G = [25 6 -20 11;
      6 4 -4 3];
 
 Cz = [-2 1 -1];
-D = 0;
-
-% G eigenvalues
-G_eig = eig(G)
-
-% A eigenvalues
-A_eig = eig(A)
-
-% Jordan matrix
-[Aj, J] = jordan(A);
-Pjre(:,1) = Aj(:,1);
-Pjre(:,2) = imag(Aj(:,2));
-Pjre(:,3) = real(Aj(:,3))
-Pjre_inv = Pjre^-1
-Aj_re = Pjre_inv * A * Pjre
-B_jre = Pjre_inv * B
+Dz = [-20 -6 16 -9];
 
 % solving Riccati
 Q = 0;
@@ -45,8 +28,8 @@ eK1=eig(A+B*K1)
 cvx_begin sdp
 variable P(3,4)
 variable Y(1,4)
-P*G-A*P == B*Y+Bf;
-Cz*P + D == 0;
+P*G-A*P == B*Y+Bg;
+Cz*P + Dz == 0;
 cvx_end
 
 K2 = Y-K1*P

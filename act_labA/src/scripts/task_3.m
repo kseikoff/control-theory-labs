ba0e782a26ded7feecf856de1a5e2cf192611665
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2025-04-20 22:29:42 +0300

    lab A task 3

--- act_labA/src/scripts/task_3.m
+++ act_labA/src/scripts/task_3.m
@@ -19,3 +19,19 @@ eig_A = eig(A)
 [P_J, A_J] = jordan(A)
 B_J = P_J^-1 * B
 C_J = C*P_J
+
+% regulator
+Qr= eye(4);
+Rr = 10*eye(2);
+
+[Pr,K,e]=icare(A,B,Qr,Rr);
+K=-inv(Rr)*B'*Pr
+ek=eig(A+B*K)
+
+% observer
+Ql= 2*eye(4);
+Rl = eye(2);
+
+[Pl,L,e]=icare(A',C',Ql,Rl);
+L=-Pl*C'*Rl^-1
+el=eig(A+L*C)
